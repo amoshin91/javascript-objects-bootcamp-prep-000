b70bb55404fe613defc2753e51e0cd2e57916e72
Yongwoo Shin <amoshinihsoma@gmail.com>
2018-11-12 20:57:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist['My Bloody Valentine'] = '2018'
+  playlist['My Bloody Valentine'] = 'Sometimes'
   return playlist;
 }
